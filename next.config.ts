30ef1d7528352e7474115f419a417c0edbd1d726
Jhaycrypt001 <alamujude25@gmail.com>
2026-04-27 17:18:14 +0100

    Fix TypeScript any error in next config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
+  webpack: (config: any) => {
     config.resolve.alias = {
       ...config.resolve.alias,
       'accounts': false, 
